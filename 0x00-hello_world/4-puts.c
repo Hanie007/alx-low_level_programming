0666a122bcca0b2b5717ebc8ee05370cc3213902
reddhanie <stephanieobaitan@gmail>
2022-09-13 07:02:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ * Description:
+
+ *'The program prints  a sentence'
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+
+  
+  return (0);
+  
+
+  
+}
